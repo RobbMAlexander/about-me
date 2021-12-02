e98a20a15fd2d8506eb120b125cadddc60cba259
Robb Alexander <RobbMAlexander@gmail.com>
2021-12-01 20:01:08 -0800

    Updated HTML & JS for lab 3

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # about-me
+
+## Project Overview
+
+This web page serves as a short autobiographical resource and a means of practicing some HTML, CSS, and JavaScript fundamentals over a few iterations.
+
+## Objectives (by Iteration)
+
+### Lab 02
+
+* Structure working HTML for a viewable page, using properly formatted tags and elements:
+`<body></body>`; `<p></p>`;`<style></style>`; `<script></script>`; etc.
+
+* Use CSS (external, in accompanying file "css/style.css") to style page for readability and distinction:
+`background-color`; `color`; etc.
+
+* Use JS (external, in accompanying file "js/app.js") to produce user prompts, then feed that input into alerts that incorporate a limited range of valid values:
+`let user = prompt('Hello! Welcome to my Bio page-- what\'s your name?')
+  alert('Hey ' + user + ', here are a few quick questions about my Bio that are explained better on the page.');`
+
+### Lab 03
+
+* Guidelines and restrictions:
+  * Use conditionals
+  * *No* functions
+
+* Add ordered list (`<ol>`) and unordered lists (`<ul>`) to/of existing content.
+
+* Add additional user prompt question that uses numeric input then evaluates input for `>` or `<` comparison, a maxiumum of 4 times, alerting user on status for each attempt and of compared value after final attempt.
+
+* Add additional user prompt question that allows for multiple values from an array, with a maximum of 6 attempts, then print array to user.
+
+* For above additions: track correct answer proportion, alert with value after all other prompts & alerts.
+
+## Licenses
+
+*Used MIT license
--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ console.log('hello world');
 let user = prompt('Hello! Welcome to my Bio page-- what\'s your name?')
   alert('Hey ' + user + ', here are a few quick questions about my Bio that are explained better on the page.');
 
+let gamescore = 0;
+
 // Question 1 - 'N' is true
 let response1 = prompt('Am I from the Seattle area?').toLowerCase();
 
@@ -14,10 +16,11 @@ let response1 = prompt('Am I from the Seattle area?').toLowerCase();
 if (response1 === 'yes' || response1 === 'y'){
   // console.log('response to q1 is "yes"');
   alert('Nope, I\'m from the Rust Belt.')
-} else if (response1 === 'no' || response1 === 'n'){
+  } else if (response1 === 'no' || response1 === 'n'){
   // console.log('response to q1 is "no"');
-  alert('That\'s right, I\'m from Upstate New York.')
-} else {
+  alert('That\'s right, I\'m from Upstate New York.');
+  gamescore++;
+  } else {
   //console.log('response to q1 is invalid')
   alert('Uh... I\'m from Syracuse.')
 };
@@ -27,11 +30,12 @@ let response2 = prompt('Can I read ancient Greek?').toLowerCase();
 
 if (response2 === 'yes' || response2 === 'y'){
   // console.log('response to q2 is "yes"');
-  alert('Yeah, I\'d need a dictionary if I looked back at anything complicated now though.')
-} else if (response2 === 'no' || response2 === 'n'){
+  alert('Yeah, I\'d need a dictionary if I looked back at anything complicated now though.');
+  gamescore++;
+  } else if (response2 === 'no' || response2 === 'n'){
   // console.log('response to q2 is "no"');
   alert('I can, actually. Yeah, some people DO spend their time and money on things like that.')
-} else {
+  } else {
   alert('I can; was that what you were trying to type?')
   // console.log('response to q2 is invalid)
 };
@@ -42,10 +46,11 @@ let response3 = prompt('Did I go to law school?').toLowerCase();
 if (response3 === 'yes' || response3 === 'y'){
   // console.log('response to q3 is "yes"');
   alert('Nope, I just ended up sticking with a job in the legal field for a while.')
-} else if (response3 === 'no' || response3 === 'n'){
+  } else if (response3 === 'no' || response3 === 'n'){
   // console.log('response to q3 is "no"');
-  alert('Right, I merely worked around lawyers for a few years.')
-} else {
+  alert('Right, I merely worked around lawyers for a few years.');
+  gamescore++;
+  } else {
   alert('Is that...latin? I studied Greek.')
   // console.log('response to q3 is invalid)
 };
@@ -54,11 +59,12 @@ if (response3 === 'yes' || response3 === 'y'){
 let response4 = prompt('Can I play the guitar?').toLowerCase();
 if (response4 === 'yes' || response4 === 'y'){
   // console.log('response to q4 is "yes"');
-  alert('Yeah, it\'s been a rewarding hobby.')
-} else if (response4 === 'no' || response4 === 'n'){
+  alert('Yeah, it\'s been a rewarding hobby.');
+  gamescore++;
+  } else if (response4 === 'no' || response4 === 'n'){
   // console.log('response to q4 is "no"');
   alert('Ouch! How well is debatable, but since I can do more than scales I\'d say "yes."')
-} else {
+  } else {
   alert('Are you doing some shredding on your keyboard there yourself?')
   // console.log('response to q4 is invalid)
 };
@@ -68,11 +74,74 @@ let response5 = prompt('Do I live in Seattle now?').toLowerCase();
 if (response5 === 'yes' || response5 === 'y'){
   // console.log('response to q5 is "yes"');
   alert('Nope-- just north of the county line.')
-} else if (response5 === 'no' || response5 === 'n'){
+  } else if (response5 === 'no' || response5 === 'n'){
   // console.log('response to q5 is "no"');
-  alert('Mountlake Terrace is pretty nice--getting light rail access in a few years too.')
-} else {
+  alert('Mountlake Terrace is pretty nice--getting light rail access in a few years too.');
+  gamescore++;
+  } else {
   alert('...Trying to guess my street address?')
   // console.log('response to q5 is invalid)
 };
-  alert('Welcome to the page, ' + user + '!');
+
+// Question 6 - "9" is true
+
+let q6guesscount = 0;
+
+while (q6guesscount < 4) {
+let response6 = prompt('How many years have I lived in the Seattle area (as of 12/01/21)? ');
+
+  // attempt at data type check below. Outside of scope of lab,so removed for now
+  // if (typeof response6 !== 'Number') {
+  //   alert('Numbers only, please');
+  //   };
+
+  if (response6 < 9) { 
+    // console.log('response 6 too low');
+    alert('Longer than that.'); 
+    q6guesscount++;
+    // prompt('How many years have I lived in the Seattle area (as of 12/01/21)? ');
+    };
+
+  if (response6 > 9) {
+    // console.log('response 6 too high');
+    alert('Not that long');
+    q6guesscount++;
+    // prompt('How many years have I lived in the Seattle area (as of 12/01/21)? ');
+    };
+
+  if (response6 == 9) {
+    //console.log('response 6 is correct')
+    alert('Yup, that\'s right.');
+    gamescore++;
+    q6guesscount = 4;
+    };
+
+  }
+
+  if (q6guesscount = 4) {
+    //console.log('alert for guesses exhausted OR correct guess')
+    alert('Nine years is enough to get a sense of the area, and to leave if I didn\'t like it.');
+};
+
+
+let q7answers = ['english', 'greek', 'french', 'spanish']
+
+let q7guesscount = 0;
+
+while (q7guesscount < 6) {
+  
+  let response7 = prompt('What\'s a language that I know?').toLowerCase();
+  
+  if (response7 === q7answers[0||1||2||3]) {
+    gamescore++;
+    alert('Yup--I\'m much better in some languages than others though.');
+    q7guesscount = 6;
+  } else {
+    q7guesscount++;
+    alert('I\'ve studied a few, but I\'m not fluent in that one');
+  };
+};
+
+alert('I\'ve studied English, Greek, French, and Spanish.');
+
+alert('You guessed ' + gamescore + ' out of 7 questions correctly. Welcome to the page, ' + user + '!');
